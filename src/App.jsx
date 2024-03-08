9437f65159d40def2c34634fd08a06763c11e102
karthikhacker <karthik.sundararajan85@gmail.com>
2024-03-08 10:22:41 +0530

    Searchbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Searchbar from './components/Searchbar'
+import './App.css';
+
+const App = () => {
+    return (
+        <div className='container'>
+            <h3>Searchbar dropdown menu using reactjs</h3>
+            <Searchbar />
+        </div>
+    )
+}
+
+export default App
